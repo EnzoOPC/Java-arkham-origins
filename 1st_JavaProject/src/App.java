dfd7ce91a23b4f0317198bc4e5d32220570e230a
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-09-11 11:43:26 -0300

    First commit
    Just seeing if things work and how to print lines/ create comments.

---
+++ 1st_JavaProject/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Batman");
+        System.out.println("I caught a little pokimon");
+        System.out.println("Batman");          // I'm learning this from Alex Lee btw
+    }
+}
