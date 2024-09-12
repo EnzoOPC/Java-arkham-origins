86fc03d725189d721e191fd35407e8fcf08a6442
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-09-11 21:27:59 -0300

    Party creation update
    Made so I can type all six pokemons in my party using a for loop ¬¬

--- User_imput/src/App.java
+++ User_imput/src/App.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Scanner scan = new Scanner(System.in);      // System.in is to make so we can type the imput in the terminal (i think)
 
-        System.out.println("Enter pokemon party member 1: ");
+        List<String> party = new ArrayList<>();     // Why do I have to write an essay just to create a fucking list ;-;
 
-        String pokemon1 = scan.nextLine();
-
-        System.out.println(pokemon1);
+        for (int i = 1; i < 7; i++) {               // for loop cool tho ¬¬
+            System.out.printf("\nPokemon number %d: \n", i);
+            String pokemon = scan.nextLine();
+            party.add(i, pokemon);
+        }
+        System.out.println("Your finished party is: " + party);
+        System.err.println();
+        System.err.println();
 
 
     }
